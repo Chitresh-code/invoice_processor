41c13ed3c5d9692084f64bc7f570c0f59e61977a
Chitresh <chitresh.gyanani@ssbi.in>
2025-01-04 22:00:21 +0530

    even more frontend changes

--- app.py
+++ app.py
@@ -14,22 +14,11 @@ hide_menu_style = """
 #MainMenu {
     visibility: hidden;
 }
-#GithubIcon {
+.stToolbarActions .stToolbarActionButton {
     visibility: hidden;
 }
-footer {
-	
-	visibility: hidden;
-	
-	}
-footer:after {
-	content:'goodbye'; 
-	visibility: visible;
-	display: block;
-	position: relative;
-	#background-color: red;
-	padding: 5px;
-	top: 2px;
+._profileContainer_gzau3_53 {
+    display: none;
 }
 </style>
 """
